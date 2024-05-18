55d5b1030f3dfe1b3e1dab62c8229ecc5192065a
Steve Song <steve.b.song92@gmail.com>
2024-05-18 16:12:52 -0400

    goquery.Selection applied

--- main.go
+++ main.go
@@ -1,24 +1,95 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
 
 func main() {
-	pages := getPages()
+	totalPages := getPages()
+	fmt.Println(totalPages)
+
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageUrl := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	fmt.Println("Requesting ", pageUrl)
+	res, err := http.Get(pageUrl)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		extractJob(card)
+	})
+
+}
+
+func extractJob(card *goquery.Selection) {
+	id, _ := card.Attr("value")
+	fmt.Println(id)
+	title := card.Find(".job_tit>a").Text()
+	fmt.Println(title)
+	location := card.Find(".job_condition> span").First().Text()
+	fmt.Println(location)
 }
 
 func getPages() int {
+
+	// req, err := http.NewRequest("GET", baseURL, nil)
+	// checkErr(err)
+
+	// purl, err := url.Parse(baseURL)
+	// checkErr(err)
+
+	// client := &http.Client{
+	// 	Transport: &http.Transport{Proxy: http.ProxyURL(purl)}}
+
+	// res, err := client.Do(req)
+	// checkErr(err)
+	// checkCode(res)
+
+	// defer res.Body.Close()
+	pages := 0
+
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
 
-	doc, err := goquery
+	defer res.Body.Close()
 
-	return 0
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+
+	// fmt.Println(doc)
+	return pages
 }
 
 func checkErr(err error) {
